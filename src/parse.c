d8daf7660b8cadc4a42deea16160f6bc3a62bb56
Lenoctambule <106790775+lenoctambule@users.noreply.github.com>
2024-09-25 08:19:08 +0200

    Added condition to locate .shstrtab

--- src/parse.c
+++ src/parse.c
@@ -16,10 +16,12 @@ void    parse64(t_file *file)
         size_t      offset  = file->ehdr64.e_shoff + i * file->ehdr64.e_shentsize;
         Elf64_Shdr  *shdr   = (Elf64_Shdr *)(file->filemap + offset);
         if (shdr->sh_type == SHT_SYMTAB)
-            printf("symbol section found at %lX\n", shdr->sh_offset);
+            printf("symbol section found at %lX and ends at %lX\n", shdr->sh_offset, shdr->sh_offset + shdr->sh_size);
         if (shdr->sh_type == SHT_DYNSYM)
-            printf("dl symbol section found at %lX\n", shdr->sh_offset);
+            printf("dl symbol section found at %lX and ends at %lX\n", shdr->sh_offset, shdr->sh_offset + shdr->sh_size);
         if (shdr->sh_type == SHT_STRTAB)
-            printf("str section found at %lX\n", shdr->sh_offset);
+            printf("str section found at %lX and ends at %lX\n", shdr->sh_offset, shdr->sh_offset + shdr->sh_size);
+        if (shdr->sh_type == SHT_STRTAB && i == file->ehdr64.e_shstrndx)
+            printf(".shstrtab section found at %lX and ends at %lX\n", shdr->sh_offset, shdr->sh_offset + shdr->sh_size);
     }
 }
